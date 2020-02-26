54395101a03c98d8fe890b91edc927ae92af6d34
root <root@localhost.localdomain>
2020-02-26 14:58:39 +0530

    update: Master

--- Assignments/01Line/main.cpp
+++ Assignments/01Line/main.cpp
@@ -10,29 +10,29 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.setWindowTitle("Assignment 1 Line Drawing");
 
-    QImage image(800, 800, QImage::Format_RGB888);
+    QImage image(1000, 700, QImage::Format_RGB888);
     QRgb value;
     value=qRgb(0,255,0);
     QImage ddaAlgorithm(int, int, int, int, QImage,QRgb);
     QImage bresenhamAlgorithm(int, int, int, int, QImage,QRgb);
 
     //outerRectangle
-    image=bresenhamAlgorithm(100,100,700,100,image,value);
-    image=bresenhamAlgorithm(100,100,100,700,image,value);
-    image=bresenhamAlgorithm(100,700,700,700,image,value);
-    image=bresenhamAlgorithm(700,100,700,700,image,value);
+    image=bresenhamAlgorithm(100,100,900,100,image,value);
+    image=bresenhamAlgorithm(900,100,900,500,image,value);
+    image=bresenhamAlgorithm(900,500,100,500,image,value);
+    image=bresenhamAlgorithm(100,500,100,100,image,value);
 
     //inner trapezium
-    image=ddaAlgorithm(100,400,400,100,image,value);
-    image=ddaAlgorithm(400,100,700,400,image,value);
-    image=ddaAlgorithm(700,400,400,700,image,value);
-    image=ddaAlgorithm(400,700,100,400,image,value);
+    image=ddaAlgorithm(100,300,500,100,image,value);
+    image=ddaAlgorithm(500,100,900,300,image,value);
+    image=ddaAlgorithm(900,300,500,500,image,value);
+    image=ddaAlgorithm(500,500,100,300,image,value);
 
     //inner Rectangle
-    image=bresenhamAlgorithm(250,250,550,250,image,value);
-    image=bresenhamAlgorithm(550,250,550,550,image,value);
-    image=bresenhamAlgorithm(550,550,250,550,image,value);
-    image=bresenhamAlgorithm(250,550,250,250,image,value);
+    image=bresenhamAlgorithm(300,200,700,200,image,value);
+    image=bresenhamAlgorithm(700,200,700,400,image,value);
+    image=bresenhamAlgorithm(700,400,300,400,image,value);
+    image=bresenhamAlgorithm(300,400,300,200,image,value);
 
     QLabel l;
     l.setPixmap(QPixmap::fromImage(image));
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 
 QImage ddaAlgorithm(int x1, int y1, int x2, int y2,QImage image,QRgb value)
 {
-    int dx,dy,steps,Xinc,Yinc,x,y;
+    float dx,dy,steps,Xinc,Yinc,x,y;
 
     dx=x2-x1;
     dy=y2-y1;
